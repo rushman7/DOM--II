bd5bef6696397c11d686eb2e9e06247652067578
BuildTools <arthurpisakhov1@gmail.com>
2019-08-27 20:44:24 -0400

    click and mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+let signBtn = document.getElementsByClassName('btn');
+for (let i = 0; i < signBtn.length;i++) {
+  signBtn[i].addEventListener('click', () => {
+    signBtn[i].innerHTML = `You're signed up!`
+  })
+}
+
+let navBtn = document.querySelectorAll('.nav-link');
+for (let i = 0; i < navBtn.length; i++) {
+  navBtn[i].addEventListener('mouseover', (e) => {
+    e.target.style.color = "purple"
+
+    setTimeout(() => {
+      e.target.style.color = "black"}, 1500);
+  })
+}
